1183e8c32c26c2765feb9515ab31e824a554092b
someone <someone@someplace.com>
2019-08-13 15:52:40 +0200

    3.4:Login, registration, bootstrap styling

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -30534,7 +30534,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49813" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49789" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
