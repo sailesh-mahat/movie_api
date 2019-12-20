366c655907ba08e0e84e4def1b9ef294da4f6d10
sailesh-mahat <sailesharsenal@gmail.com>
2019-12-20 11:41:09 +0100

    5.8: added jsdoc

---
+++ client-2/build/precache-manifest.795a987d99960d129d3bf3c29e40e9df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82f0ba1464affb31f92f99fbb18aaa60",
+    "url": "/movie_api/index.html"
+  },
+  {
+    "revision": "2250470aa883823d4afc",
+    "url": "/movie_api/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "fb94bc461f41984719ee",
+    "url": "/movie_api/static/css/main.1da5548e.chunk.css"
+  },
+  {
+    "revision": "2250470aa883823d4afc",
+    "url": "/movie_api/static/js/2.15cbe913.chunk.js"
+  },
+  {
+    "revision": "fb94bc461f41984719ee",
+    "url": "/movie_api/static/js/main.24b4c7da.chunk.js"
+  },
+  {
+    "revision": "58d1397a2e2025d96233",
+    "url": "/movie_api/static/js/runtime~main.aa89b72b.js"
+  }
+]);
